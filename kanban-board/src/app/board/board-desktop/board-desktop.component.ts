8f506b328ff35b1b88d3eebce6ffbd0532523290
SupreetOjas <ojassupreet@Supreets-MacBook-Pro.local>
2020-04-24 00:39:22 +0530

    desktop board initial commit

---
+++ kanban-board/src/app/board/board-desktop/board-desktop.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-board-desktop',
+  templateUrl: './board-desktop.component.html',
+  styleUrls: ['./board-desktop.component.scss']
+})
+export class BoardDesktopComponent implements OnInit {
+
+  @Input() data=[];
+ 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- kanban-board/src/app/board/board.module.ts
+++ kanban-board/src/app/board/board.module.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { BoardContainerComponent } from './board-container/board-container.component';
 import { BoardMobileComponent } from './board-mobile/board-mobile.component';
 import {MatTabsModule} from '@angular/material/tabs';
+import { BoardDesktopComponent } from './board-desktop/board-desktop.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 const routes = [
   {
@@ -14,12 +16,13 @@ const routes = [
 ];
 
 @NgModule({
-  declarations: [BoardContainerComponent, BoardMobileComponent],
+  declarations: [BoardContainerComponent, BoardMobileComponent, BoardDesktopComponent],
   imports: [
     CommonModule,
     SharedModule,
     MatTabsModule,
     RouterModule.forChild(routes),
+    DragDropModule
   ],
   exports : [RouterModule]
 })
