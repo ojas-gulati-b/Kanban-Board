c3506f7344de948f646fd9cab23311c51a82f6a6
ojas-gulati-b <ojasgulati-hpe@users.noreply.github.com>
2020-04-25 18:06:14 +0530

    added add deal functionality to desktop view

--- kanban-board/src/app/board/board-desktop/board-desktop.component.ts
+++ kanban-board/src/app/board/board-desktop/board-desktop.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { DealData } from '../board-container/board-container.component';
 
 @Component({
   selector: 'app-board-desktop',
@@ -8,8 +9,14 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 })
 export class BoardDesktopComponent implements OnInit {
 
-  @Input() data=[];
+  @Input() data = [];
+  @Output() addDeal = new EventEmitter<DealData>();
  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -21,13 +28,15 @@ export class BoardDesktopComponent implements OnInit {
     }
   }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  addStage(){
-    return confirm('stage added');
+  onAddDeal(stageIndex) {
+    this.addDeal.emit({
+      stageID: this.data[stageIndex].stageID,
+      deal: {
+        dealID: 'DealNew',
+        dealTitle: 'New Deal',
+        dealAmount: 'Rs 5000'
+      },
+    });
   }
 
 }
